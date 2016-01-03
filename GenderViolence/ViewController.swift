a45900daeae9ac30f5c27075f31251936ff6e10c
Mauricio Taumaturgo <mto@cin.ufpe.br>
2016-01-03 18:23:45 -0300

    First Commit

---
+++ GenderViolence/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GenderViolence
+//
+//  Created by Mauricio Oliveira on 12/28/15.
+//  Copyright (c) 2015 Mauricio Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
