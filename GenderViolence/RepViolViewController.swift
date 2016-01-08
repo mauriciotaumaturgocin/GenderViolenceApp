33f1bec6f470371fc4983b3391c4cb297a349958
Mauricio Taumaturgo <mto@cin.ufpe.br>
2016-01-08 00:49:55 -0300

    Saving Data

--- GenderViolence/RepViolViewController.swift
+++ GenderViolence/RepViolViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class RepViolViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
+    
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    
+    @IBOutlet weak var stateTextField: UITextField!
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    @IBOutlet weak var streetTextField: UITextField!
+    
+    @IBOutlet weak var violenceTypeSegControl: UISegmentedControl!
+    
+    @IBOutlet weak var isVictimSegControl: UISegmentedControl!
+    
+    @IBOutlet weak var isDomesticViolenceSegControl: UISegmentedControl!
+    
     @IBOutlet weak var pickerViolenceType: UIPickerView!
     
     @IBOutlet weak var pickerViolenceFrequency: UIPickerView!
@@ -56,6 +71,36 @@ class RepViolViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
             return pickerFrequencyData[row]
         }
         return pickerViolenceTypeData[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // This method is triggered whenever the user makes a change to the picker selection.
+        // The parameter named row and component represents what was selected.
+    }
+    
+    
+    @IBAction func reportClicked(sender: AnyObject) {
+        
+        var defaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        
+        defaults.setObject(self.zipCodeTextField.text, forKey: "zipCode")
+        defaults.setObject(self.cityTextField.text, forKey: "city")
+        defaults.setObject(self.stateTextField.text, forKey: "state")
+        defaults.setObject(self.streetTextField.text, forKey: "street")
+        defaults.setObject(violenceTypeSegControl.titleForSegmentAtIndex(isVictimSegControl.selectedSegmentIndex), forKey: "violenceType")
+        defaults.setObject(isVictimSegControl.titleForSegmentAtIndex(isVictimSegControl.selectedSegmentIndex), forKey: "isVictim")
+        defaults.setObject(isDomesticViolenceSegControl.titleForSegmentAtIndex(isDomesticViolenceSegControl.selectedSegmentIndex), forKey: "isDomesticViolence")
+        
+        var text = pickerViolenceTypeData[pickerViolenceType.selectedRowInComponent(0)]
+        
+        defaults.setObject(text, forKey: "violenceGender")
+        defaults.setObject(pickerFrequencyData[pickerViolenceFrequency.selectedRowInComponent(0)], forKey: "violenceFrequency")
+        
+        
+        defaults.synchronize()
+        
+        print(text)
+        print("saved")
     }
     
     
---
+++ GenderViolence/ViolenceReport.swift
@@ -0,0 +1,21 @@
+//
+//  ViolenceReport.swift
+//  GenderViolence
+//
+//  Created by Mauricio Oliveira on 1/7/16.
+//  Copyright (c) 2016 Mauricio Oliveira. All rights reserved.
+//
+
+import Foundation
+
+
+class ViolenceReport: NSData {
+    
+    
+    
+    
+    
+    
+    
+    
+}
